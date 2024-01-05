3b3d77bb4137cdbfb06c9d7f90834c2206a0edd2
aoroudjev <a.oroudjev@gmail.com>
2024-01-05 00:55:06 -0800

    Allow headless mode.

--- driver_handler.py
+++ driver_handler.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import undetected_chromedriver as uc
 
-def start_driver() -> webdriver:
+def start_driver():
     # returns headless driver
     options = Options()
-    options.headless = True
+    options.headless = False
     options.EnableVerboseLogging = True
     return uc.Chrome(options=options)
