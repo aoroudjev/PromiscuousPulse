d2ae331125b80ed24520257a1918f38340adadb1
aoroudjev <a.oroudjev@gmail.com>
2024-01-05 00:56:19 -0800

    Add login() functionality.

--- web_handler.py
+++ web_handler.py
@@ -3,10 +3,11 @@ import time
 from undetected_chromedriver import By
 
 
-def login(driver):
+def login(driver, credentials):
     driver.get('https://app.member.virginpulse.com/#/home')
-    time.sleep(10)
-    input_elements = [elem for elem in driver.find_elements(By.TAG_NAME, 'input')]
-    print(len(input_elements))
+    time.sleep(5)
+    input_elements = [elem for elem in driver.find_elements(By.TAG_NAME, 'input') if
+                      elem.accessible_name.lower() in credentials]
     for i in input_elements:
-        print(i.accessible_name)
+        i.send_keys(credentials[str(i.accessible_name.lower())])
+    driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
