06fd3556c968ff29940a61b8d828ede4447ad45d
aoroudjev <a.oroudjev@gmail.com>
2024-01-07 21:22:29 -0800

    Add "All" button.

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ credentials = {}
 def update_entry_state(state) -> None:
     username_entry.configure(state=state)
     password_entry.configure(state=state)
+    first_check.configure(state=state)
     start_button.configure(state=state)
+
+
+def update_options_state(state) -> None:
+    dailies_button.configure(state=state)
+    weeklies_button.configure(state=state)
+    monthlies_button.configure(state=state)
+    all_button.configure(state=state)
 
 
 def run_thread() -> None:
@@ -30,17 +38,20 @@ def run_thread() -> None:
 
 
 def full_sequence():
+    # TODO: Change to separate events for button functionality
     update_status('ORANGE', 'Starting Driver...')
     driver = driver_handler.start_driver()
     driver.set_window_size(100, 500)
     update_status('ORANGE', 'Logging In')
     try:
         web_handler.login(driver, credentials)
-        update_status('GREEN', 'Success')
+        update_status('GREEN', 'Success...Ready.')
     except ValueError:
         driver.close()
         update_status('RED', 'Login Failed')
         update_entry_state('normal')
+    update_options_state('normal')
+    web_handler.do_dailies(driver)
 
 
 def update_status(color, text) -> None:
@@ -48,7 +59,7 @@ def update_status(color, text) -> None:
 
 
 root = tk.Tk()
-root.title('PromiscuousPulse')
+root.title('Pulse')
 root.geometry('220x200')
 root.resizable(width=False, height=False)
 
@@ -56,12 +67,13 @@ root.resizable(width=False, height=False)
 username_label = tk.Label(root, text="Username:")
 username_label.grid(column=0, row=0, sticky=tk.E)
 username_entry = tk.Entry(root, width=25)
-username_entry.insert(0, "@wyatt.com")
+username_entry.insert(0, "aoroudjev@wyatt.com")
 username_entry.grid(column=1, row=0)
 
 password_label = tk.Label(root, text="Password:")
 password_label.grid(column=0, row=1, sticky=tk.E)
 password_entry = tk.Entry(root, show="*", width=25)  # Password is hidden
+password_entry.insert(0, "Nickshell123!")
 password_entry.grid(column=1, row=1)
 
 # Control Buttons
@@ -73,17 +85,20 @@ status_label = tk.Label(root, fg='GREEN', text='READY', font='BOLD', borderwidth
 status_label.grid(column=0, row=4, columnspan=2)
 
 first_var = tk.BooleanVar()
-first_check = tk.Checkbutton(root, text='First Time', variable=first_var, borderwidth=2, relief='solid')
+first_check = tk.Checkbutton(root, text='First Login', variable=first_var, borderwidth=2, relief='solid')
 first_check.grid(column=0, row=3, columnspan=2, sticky=tk.W)
 
 # Options
-option_lf = ttk.LabelFrame(root, text='Options')
+option_lf = tk.LabelFrame(root, text='Options')
 option_lf.grid(column=0, row=5, padx=2, columnspan=3)
 
-dailies_check = tk.Button(option_lf, text='Dailies', state='disabled')
-dailies_check.grid(column=0, row=0, padx=5, pady=5)
-weeklies_check = tk.Button(option_lf, text='Weeklies', state='disabled')
-weeklies_check.grid(column=1, row=0, padx=5, pady=5)
-monthlies_check = tk.Button(option_lf, text='Monthlies', state='disabled')
-monthlies_check.grid(column=2, row=0, padx=5, pady=5)
+dailies_button = tk.Button(option_lf, text='Dailies', state='disabled', command="")
+dailies_button.grid(column=0, row=0, padx=5, pady=5)
+weeklies_button = tk.Button(option_lf, text='Weeklies', state='disabled', command="")
+weeklies_button.grid(column=1, row=0, padx=5, pady=5)
+monthlies_button = tk.Button(option_lf, text='Monthlies', state='disabled', command="")
+monthlies_button.grid(column=2, row=0, padx=5, pady=5)
+all_button = tk.Button(option_lf, text='ALL', state='disabled', command="")
+all_button.grid(column=1, row=1, padx=5, pady=5)
+
 root.mainloop()
